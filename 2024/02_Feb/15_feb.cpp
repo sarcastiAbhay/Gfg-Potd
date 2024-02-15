f97f70260490b1e49aa6aca4f0c7ee3d2e6b817c
ABHAY KUMAR <92247068+sarcastiAbhay@users.noreply.github.com>
2024-02-15 21:19:19 +0530

    Update 15_feb.cpp

--- 2024/02_Feb/15_feb.cpp
+++ 2024/02_Feb/15_feb.cpp
@@ -35,11 +35,6 @@ public:
 ### Contribution and Support
 
 I always encourage contributors to participate in the discussion forum for this repository.
-
-If you have a better solution or any queries / discussions related to the `Problem of the Day` solution, please visit our [discussion section](https://github.com/getlost01/gfg-potd/discussions). We welcome your input and aim to foster a collaborative learning environment.
-
-If you find this solution helpful, consider supporting us by giving a `⭐ star` to the [getlost01/gfg-potd](https://github.com/getlost01/gfg-potd) repository.
-
-![Total number of repository visitors](https://komarev.com/ghpvc/?username=gl01potdgfg&color=blue&&label=Visitors)
-![](https://hit.yhype.me/github/profile?user_id=79409258)
+ 
+If you find this solution helpful, consider supporting us by giving a `⭐ star` .
 
