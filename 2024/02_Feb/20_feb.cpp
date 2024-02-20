6e0ea96aaa31e904de16e2a9140c7da579a4acd4
ABHAY KUMAR <92247068+sarcastiAbhay@users.noreply.github.com>
2024-02-20 18:02:52 +0530

    Add files via upload

---
+++ 2024/02_Feb/20_feb.cpp
@@ -0,0 +1,40 @@
+//User function template for C++
+
+// s : given string to search
+// dictionary : vector of available strings
+class Solution
+{
+public:
+    unordered_set<string> st;
+    bool solve(int idx, int n, string s,  vector<string> &dictionary)
+    {
+        if(idx==n)
+        {
+            return true;
+        }
+        
+        if(st.find(s)!=st.end())
+        {
+            return true;
+        }
+        
+        for(int l=0; l<n; l++)
+        {
+            string temp=s.substr(idx, l);
+            if(st.find(temp) != st.end() && solve(idx+l, n, s, dictionary))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    int wordBreak(int n, string s, vector<string> &dictionary) {
+        //code here
+        int size=s.length();
+        for(auto word: dictionary)
+        {
+            st.insert(word);
+        }
+        return solve(0,size, s, dictionary);
+    }
+};
